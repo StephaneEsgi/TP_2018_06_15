5244b8d5b97a2703875fd9db09b9983f4301438a
Stéphane Guardo <stephaneesgi@gmail.com>
2018-06-15 11:16:35 +0200

    Initialisation des projets des 11 Exercices

---
+++ Exercice8/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercice8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
